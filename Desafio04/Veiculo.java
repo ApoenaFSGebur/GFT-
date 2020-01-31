ec44505711f3d2d706376d98b1c56881e23dea8b
ApoenaFSGebur <60357108+ApoenaFSGebur@users.noreply.github.com>
2020-01-31 16:00:00 -0300

    Arquivo adicionado

---
+++ Desafio04/Veiculo.java
@@ -0,0 +1,56 @@
+package main;
+
+public  abstract class Veiculo {
+	private String modelo;
+	private int velocidade;
+	private int passageiros;
+	private int combustivel;
+	
+	public Veiculo(String modelo, int velocidade, int passageiros, int combustivel) {
+		super();
+		this.modelo = modelo;
+		this.velocidade = velocidade;
+		this.passageiros = passageiros;
+		this.combustivel = combustivel;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getVelocidade() {
+		return velocidade;
+	}
+
+	public void setVelocidade(int velocidade) {
+		this.velocidade = velocidade;
+	}
+
+	public int getPassageiros() {
+		return passageiros;
+	}
+
+	public void setPassageiros(int passageiros) {
+		this.passageiros = passageiros;
+	}
+
+	public int getCombustivel() {
+		return combustivel;
+	}
+
+	public void setCombustivel(int combustivel) {
+		this.combustivel = combustivel;
+	}
+
+	@Override
+	public String toString() {
+		return "Veiculo [modelo=" + modelo + ", velocidade=" + velocidade + ", passageiros=" + passageiros
+				+ ", combustivel=" + combustivel + "]";
+	}
+	
+	
+}
