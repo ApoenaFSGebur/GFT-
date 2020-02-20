194561c628564b894de9aecd869d3e5d13f464e5
ApoenaFSGebur <apoenagebur@gmail.com>
2020-02-20 15:38:12 -0300

    Adicionado os arquivos

--- casadeshow/src/main/java/com/gerenciadordeeventos/casadeshow/EventosController.java
+++ casadeshow/src/main/java/com/gerenciadordeeventos/casadeshow/EventosController.java
@@ -6,17 +6,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EventosController {
 
+	@RequestMapping("/Gerenciador")
+	public String getGerenciadorDeEventos() {
+		return "GerenciadorDeEventos";
+	}
+	
+	@RequestMapping("/")
+	public String getPaginaInicial() {
+		return "PaginaInicial";
+	}
+	
+	@RequestMapping("/CasadeShow")
+	public String getCadastroCasaDeShow() {
+		return "CadastroCasaDeShow";		
+	}
+
 	@RequestMapping("/evento")
 	public String getCadastroEventos() {
 		return "CadastroEventos";
 	}
-		@RequestMapping("/CasadeShow")
-		public String getCadastroCasaDeShow() {
-			return "CadastroCasaDeShow";		
-	}
-		@RequestMapping("/")
-		public String getPaginaInicial() {
-			return "PaginaInicial";
-		}
-
+		
 }
+		
