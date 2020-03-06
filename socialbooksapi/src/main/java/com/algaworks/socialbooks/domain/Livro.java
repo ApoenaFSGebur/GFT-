6495c04e0a8a73aa243ac87dde59ecea72f3abcd
ApoenaFSGebur <apoenagebur@gmail.com>
2020-03-06 15:36:27 -0300

    Adicionado os arquivos

--- socialbooksapi/src/main/java/com/algaworks/socialbooks/domain/Livro.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/domain/Livro.java
@@ -8,8 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Transient;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -24,6 +28,7 @@ public class Livro {
 	private String nome;
 	
 	@JsonInclude(Include.NON_NULL)
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date publicacao;
 	
 	@JsonInclude(Include.NON_NULL)
@@ -32,61 +37,74 @@ public class Livro {
 	@JsonInclude(Include.NON_NULL)
 	private String resumo;
 	
-	@JsonInclude(Include.NON_NULL)
-	@Transient
+	@JsonInclude(Include.NON_EMPTY)
+	@OneToMany(mappedBy = "livro")
 	private List<Comentario> comentarios;
 	
+	@ManyToOne
+	@JoinColumn(name = "AUTOR_ID")
 	@JsonInclude(Include.NON_NULL)
-	private String autor;
+	private Autor autor;
 	
 	public Livro() {}
-
+	
 	public Livro(String nome) {
 		this.nome = nome;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public Date getPublicacao() {
 		return publicacao;
 	}
+
 	public void setPublicacao(Date publicacao) {
 		this.publicacao = publicacao;
 	}
+
 	public String getEditora() {
 		return editora;
 	}
+
 	public void setEditora(String editora) {
 		this.editora = editora;
 	}
+
 	public String getResumo() {
 		return resumo;
 	}
+
 	public void setResumo(String resumo) {
 		this.resumo = resumo;
 	}
+
 	public List<Comentario> getComentarios() {
 		return comentarios;
 	}
+
 	public void setComentarios(List<Comentario> comentarios) {
 		this.comentarios = comentarios;
 	}
-	public String getAutor() {
+
+	public Autor getAutor() {
 		return autor;
 	}
-	public void setAutor(String autor) {
+
+	public void setAutor(Autor autor) {
 		this.autor = autor;
 	}
-	
-	
 }
- 
--- socialbooksapi/src/main/java/com/algaworks/socialbooks/service/LivrosService.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/service/LivrosService.java
@@ -1,12 +1,16 @@
 package com.algaworks.socialbooks.service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.socialbooks.domain.Comentario;
 import com.algaworks.socialbooks.domain.Livro;
+import com.algaworks.socialbooks.repository.ComentariosRepository;
 import com.algaworks.socialbooks.repository.LivrosRepository;
 import com.algaworks.socialbooks.services.exceptions.LivroNaoEncontradoException;
 
@@ -15,44 +19,65 @@ public class LivrosService {
 
 	@Autowired
 	private LivrosRepository livrosRepository;
-	
+
+	@Autowired
+	private ComentariosRepository comentariosRepository;
+
 	public List<Livro> listar() {
 		return livrosRepository.findAll();
 	}
-	
+
 	public Livro buscar(Long id) {
-		Livro livro = livrosRepository.findOne(id);
-		
-		if(livro == null) {
-			throw new LivroNaoEncontradoException("O livro não pode ser encontrado!");
+		Optional<Livro> livro =  livrosRepository.findById(id);
+
+		if(!livro.isPresent()) {
+			return livro.get();
+		}else {
+			
+			 throw new LivroNaoEncontradoException("O livro não pode ser encontrado!");
 		}
-		
-		return livro; 
+
 	}
-	
+
 	public Livro salvar(Livro livro) {
 		livro.setId(null);
 		return livrosRepository.save(livro);
-		
+
 	}
-	
+
 	public void deletar(Long id) {
 		try {
-			livrosRepository.delete(id);
+			livrosRepository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new LivroNaoEncontradoException("O livro não pode ser encontrado!");
 		}
 	}
-	
+
 	public void atualizar(Livro livro) {
 		verificarExistencia(livro);
 		livrosRepository.save(livro);
 	}
-	
+
 	private void verificarExistencia(Livro livro) {
 		buscar(livro.getId());
-		}
 	}
+
+	public Comentario salvarComentario(Long livroId, Comentario comentario) {
+		Livro livro = buscar(livroId);
+
+		comentario.setLivro(livro);
+		comentario.setData(new Date());
+
+		return comentariosRepository.save(comentario);
+	}
+	
+	public List<Comentario> listarComentarios(Long livroId) {
+		Livro livro = buscar(livroId);
+		
+		return livro.getComentarios();
+	}
+}
+
 		
 		
 
