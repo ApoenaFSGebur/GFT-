cd87e82501bcbd232a5251746f7e5451af64a3e3
ApoenaFSGebur <apoenagebur@gmail.com>
2020-03-16 15:55:42 -0300

    Adicionado os arquivos

---
+++ huntweb/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import api from './services/api';
+
+import "./styles.css";
+
+import Header from './components/Header';
+
+
+const App = () => (
+	<div className="App">
+		<Header />	
+	</div>
+);	
+	
+export default App;
+     
