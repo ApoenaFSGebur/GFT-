5fbdfc8d5a8748f82f299e9d24900554a1b81066
ApoenaFSGebur <apoenagebur@gmail.com>
2020-03-17 15:49:10 -0300

    ok

--- huntweb/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
