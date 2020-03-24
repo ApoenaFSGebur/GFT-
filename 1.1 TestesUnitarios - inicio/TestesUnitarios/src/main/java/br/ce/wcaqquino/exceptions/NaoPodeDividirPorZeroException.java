357bb8e6547a317eb78aeb6254149e934b0654c3
ApoenaFSGebur <apoenagebur@gmail.com>
2020-03-24 09:48:41 -0300

    adicionado os arquivos finais

---
+++ 1.1 TestesUnitarios - inicio/TestesUnitarios/src/main/java/br/ce/wcaqquino/exceptions/NaoPodeDividirPorZeroException.java
@@ -0,0 +1,10 @@
+package br.ce.wcaqquino.exceptions;
+
+public class NaoPodeDividirPorZeroException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5608580009163762245L;
+
+}
